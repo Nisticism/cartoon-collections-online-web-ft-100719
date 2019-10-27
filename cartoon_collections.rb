ab146720ddebb537e3aa12912d2a386f0d1132bb
Hans Foster <fosterhans@gmail.com>
2019-10-27 19:38:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index
+    puts "#{dwarves[i]}"
+  end
 end
 
 def summon_captain_planet# code an argument here
